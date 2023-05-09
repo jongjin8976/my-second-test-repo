287ae420f297f3ba9aa65f4ab050b313acf8737e
jongjin8976 <whdwls8976@gmail.com>
2023-05-09 15:01:33 +0900

    집에서 작업함

--- src/test/Test1.java
+++ src/test/Test1.java
@@ -4,5 +4,6 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("프로그램 시작");
 		System.out.println("프로그램 끝~~~");
+		System.out.println("주말공부하기시러요~ 집에서 작업함");
 	}
 }
